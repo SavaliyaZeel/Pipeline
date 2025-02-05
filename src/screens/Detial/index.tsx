c047105d645cecb4b6f8aaf98e0c65d888b408c8
savaliya zeel <Savaliyazeel12@gmail.com>
2025-02-05 11:56:58 +0530

    detail UI

--- src/screens/Detial/index.tsx
+++ src/screens/Detial/index.tsx
@@ -1,33 +1,12 @@
 import { Text, View } from 'react-native';
 import React from 'react';
-import { styles } from './index.styles';
 
-interface DetialProps {
-  route: {
-    params: {
-      id: number;
-      name: string;
-      data: {
-        color: string;
-        capacity: string;
-      };
-    };
-  };
-}
-
-const Detial = ({ route }: DetialProps) => {
-  const data = route.params;
-
+const index = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.detials}>
-        <Text>ID : {data.id}</Text>
-        <Text>name : {data.name}</Text>
-        <Text>color : {data.data.color}</Text>
-        <Text>capacity : {data.data.capacity}</Text>
-      </View>
+    <View>
+      <Text>index</Text>
     </View>
   );
 };
 
-export default Detial;
+export default index;
