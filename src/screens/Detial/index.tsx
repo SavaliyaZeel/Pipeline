4770ebb734666d5b1db5599ecad3897b7222cd42
savaliya zeel <Savaliyazeel12@gmail.com>
2025-02-05 18:45:09 +0530

    Update index.tsx

--- src/screens/Detial/index.tsx
+++ src/screens/Detial/index.tsx
@@ -1,9 +1,33 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
 import { styles } from './index.styles';
 
-const Detial = () => {
-  return <View style={styles.container}></View>;
+interface DetialProps {
+  route: {
+    params: {
+      id: number;
+      name: string;
+      data: {
+        color: string;
+        capacity: string;
+      };
+    };
+  };
+}
+
+const Detial = ({ route }: DetialProps) => {
+  const { data } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.detials}>
+        <Text>ID : {data.id}</Text>
+        <Text>name : {data.name}</Text>
+        <Text>color : {data.data.color}</Text>
+        <Text>capacity : {data.data.capacity}</Text>
+      </View>
+    </View>
+  );
 };
 
 export default Detial;
